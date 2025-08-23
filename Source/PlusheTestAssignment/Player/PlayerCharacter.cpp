6728fd826d88d9ea64def1037a946453300704be
Dimaster <gordeev.dmitriy@physics.msu.ru>
2025-08-23 18:04:40 +0300

    perception

---
+++ Source/PlusheTestAssignment/Player/PlayerCharacter.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Setup MappingContext
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			if (InputMappingContext)
+				Subsystem->AddMappingContext(InputMappingContext, 0); // 0 is priority
+		}
+	}
+	
+}
+
+void APlayerCharacter::MoveInputTriggered(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::MoveInputCompleted(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::LookInputTriggered(const FInputActionValue& Value)
+{
+}
+
+void APlayerCharacter::LookInputCompleted(const FInputActionValue& Value)
+{
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/PlusheTestAssignment/Player/PlayerCharacter.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class PLUSHETESTASSIGNMENT_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/* Input handlers --------------------------------------------  */
+	virtual void MoveInputTriggered(const FInputActionValue& Value);
+	virtual void MoveInputCompleted(const FInputActionValue& Value);
+	
+	virtual void LookInputTriggered(const FInputActionValue& Value);
+	virtual void LookInputCompleted(const FInputActionValue& Value);
+	
+	
+	/* Input ------------------------- */
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput", meta = (AllowPrivateAccess = "true"))
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput", meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+};
